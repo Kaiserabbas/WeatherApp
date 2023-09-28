0dee0a39030a6ad67e111e98ad32d2179b960152
Qaisar <kayser.abbas@gmail.com>
2023-09-28 10:55:27 +0500

    Change the back button

--- src/components/CardDetails.js
+++ src/components/CardDetails.js
@@ -14,13 +14,30 @@ const CardDetails = () => {
   };
 
   return (
-    <div>
-      <button onClick={handleBackClick}>Back</button> {/* Back button */}
+    <div className='details'>
+      <div onClick={handleBackClick} className='back'>  {"<"} </div> {/* Back button */}
       <h1>{city} Weather Details</h1>
       {weatherData && (
         <div>
+          <h2>{weatherData.location.name}</h2>
+          <h3>{weatherData.location.country}</h3>
+          <img src={weatherData.current.condition.icon} alt={weatherData.current.condition.text} />
+          <p>{weatherData.current.condition.text}</p>
+          <h3 className='time'>{weatherData.location.localtime}</h3>
           <p>
             <strong>Temperature:</strong> {weatherData.current.temp_c}°C
+          </p>
+          <p>
+            <strong>Feels like:</strong> {weatherData.current.feelslike_c}°C
+          </p>
+          <p>
+            <strong>Rain:</strong> {weatherData.current.precip_mm} mm
+          </p>
+          <p>
+            <strong> UV radiation:</strong> {weatherData.current.uv} mW/cm2
+          </p>
+          <p>
+            <strong>Clouds:</strong> {weatherData.current.cloud} %
           </p>
           <p>
             <strong>Wind:</strong> {weatherData.current.wind_kph} km/h
