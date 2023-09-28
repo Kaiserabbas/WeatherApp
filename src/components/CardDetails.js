28a760e50f557916c394683659360f28f1bdc182
Qaisar <kayser.abbas@gmail.com>
2023-09-28 06:27:54 +0500

    Add back button

--- src/components/CardDetails.js
+++ src/components/CardDetails.js
@@ -1,38 +1,42 @@
+// src/components/CardDetails.js
 import React from 'react';
 import { useSelector } from 'react-redux';
-import Modal from 'react-bootstrap/Modal';
+import { useParams, useNavigate } from 'react-router-dom'; // Import useNavigate
 
-const CardDetails = ({ city, show, onHide }) => {
+const CardDetails = () => {
+  const { city } = useParams();
   const weatherData = useSelector((state) => state.weather.weatherData[city]);
+  const navigate = useNavigate(); // Initialize useNavigate
+
+  // Function to handle back button click
+  const handleBackClick = () => {
+    navigate('/'); // Navigate back to the main page
+  };
 
   return (
-    <Modal show={show} onHide={onHide} centered>
-      <Modal.Header closeButton>
-        <Modal.Title>{city} Weather Details</Modal.Title>
-      </Modal.Header>
-      <Modal.Body>
-        {weatherData && (
-          <div>
-            <p>
-              <strong>Temperature:</strong> {weatherData.current.temp_c}°C
-            </p>
-            <p>
-              <strong>Wind:</strong> {weatherData.current.wind_kph} km/h
-            </p>
-            <p>
-              <strong>Humidity:</strong> {weatherData.current.humidity}%
-            </p>
-            <p>
-              <strong>Visibility:</strong> {weatherData.current.vis_km} km    
-            </p>
-            <p>
-              <strong>Pressure:</strong> {weatherData.current.pressure_mb} mb
-            </p>
-              
-          </div>
-        )}
-      </Modal.Body>
-    </Modal>
+    <div>
+      <button onClick={handleBackClick}>Back</button> {/* Back button */}
+      <h1>{city} Weather Details</h1>
+      {weatherData && (
+        <div>
+          <p>
+            <strong>Temperature:</strong> {weatherData.current.temp_c}°C
+          </p>
+          <p>
+            <strong>Wind:</strong> {weatherData.current.wind_kph} km/h
+          </p>
+          <p>
+            <strong>Humidity:</strong> {weatherData.current.humidity}%
+          </p>
+          <p>
+            <strong>Visibility:</strong> {weatherData.current.vis_km} km
+          </p>
+          <p>
+            <strong>Pressure:</strong> {weatherData.current.pressure_mb} mb
+          </p>
+        </div>
+      )}
+    </div>
   );
 };
 
