83859405a3a7ee59bf8677f29741fee12fafa5d1
Qaisar <kayser.abbas@gmail.com>
2023-09-28 05:43:30 +0500

    Created store

---
+++ src/redux/store.js
@@ -0,0 +1,11 @@
+// store.js
+import { configureStore } from '@reduxjs/toolkit';
+import weatherReducer from './weatherSlice';
+
+const store = configureStore({
+  reducer: {
+    weather: weatherReducer,
+  },
+});
+
+export default store;
